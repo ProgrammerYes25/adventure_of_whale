7b0e8f66ede5d44b4943d9d7e3b29360e9bbe0b4
yejin4117 <s2108@e-mirim.hs.kr>
2022-11-26 17:12:59 +0900

    BottomSeaweed,Whale 틀 잡기

--- src/whale_adventure/BottomSeaweed.java
+++ src/whale_adventure/BottomSeaweed.java
@@ -1,4 +1,61 @@
 package whale_adventure;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 public class BottomSeaweed {
+    private Image bottomSeaweed;
+    private int xLoc = 0, yLoc = 0;
+
+    public BottomSeaweed(int initialWidth, int initialHeight) {
+        bottomSeaweed = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("resources/tube_bottom.png"));
+        scaleBottomSeaweed(initialWidth, initialHeight);
+    }
+
+    public void scaleBottomSeaweed(int width, int height) {
+        bottomSeaweed = bottomSeaweed.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+    }
+
+    public Image getSeaweed() {
+        return bottomSeaweed;
+    }
+
+    public int getWidth() {
+        return bottomSeaweed.getWidth(null);
+    }
+
+    public int getHeight() {
+        return bottomSeaweed.getHeight(null);
+    }
+
+    public void setX(int x) {
+        xLoc = x;
+    }
+
+    public int getX() {
+        return xLoc;
+    }
+
+    public void setY(int y) {
+        yLoc = y;
+    }
+
+    public int getY() {
+        return yLoc;
+    }
+
+    public Rectangle getRectangle() {
+        return (new Rectangle(xLoc, yLoc, bottomSeaweed.getWidth(null), bottomSeaweed.getHeight(null)));
+    }
+
+    public BufferedImage getBI() {
+        BufferedImage bi = new BufferedImage(bottomSeaweed.getWidth(null), bottomSeaweed.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics g = bi.getGraphics();
+        g.drawImage(bottomSeaweed, 0, 0, null);
+        g.dispose();
+        return bi;
+    }
 }
--- src/whale_adventure/Whale.java
+++ src/whale_adventure/Whale.java
@@ -1,4 +1,69 @@
 package whale_adventure;
 
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 public class Whale {
+    private Image flappyWhale;
+    private int xLoc = 0, yLoc = 0;
+
+    public Whale(int initialWidth, int initialHeight) {
+        flappyWhale = Toolkit.getDefaultToolkit().getImage(this.getClass().getResource("resources/blue_bird.png"));
+        scaleWhale(initialWidth, initialHeight);
+    }
+
+    public void scaleWhale(int width, int height) {
+        flappyWhale = flappyWhale.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+    }
+
+    public Image getWhale() {
+        return flappyWhale;
+    }
+
+    public int getWidth() {
+        try {
+            return flappyWhale.getWidth(null);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public int getHeight() {
+        try {
+            return flappyWhale.getHeight(null);
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public void setX(int x) {
+        xLoc = x;
+    }
+
+    public int getX() {
+        return xLoc;
+    }
+
+    public void setY(int y) {
+        yLoc = y;
+    }
+
+    public int getY() {
+        return yLoc;
+    }
+
+    public Rectangle getRectangle() {
+        return (new Rectangle(xLoc, yLoc, flappyWhale.getWidth(null), flappyWhale.getHeight(null)));
+    }
+
+    public BufferedImage getBI() {
+        BufferedImage bi = new BufferedImage(flappyWhale.getWidth(null), flappyWhale.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics g = bi.getGraphics();
+        g.drawImage(flappyWhale, 0, 0, null);
+        g.dispose();
+        return bi;
+    }
 }
