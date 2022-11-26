7a2d64c155f22aba3d45edb3c42f46e3b37ca7c8
yscho010612@gmail.com <yscho010612@gmail.com>
2022-11-26 23:04:01 +0900

    데이터 베이스 테스트 코드

---
+++ src/whale_adventure/test.java
@@ -0,0 +1,28 @@
+package whale_adventure;
+
+import com.mysql.cj.x.protobuf.MysqlxSql;
+import org.omg.PortableInterceptor.USER_EXCEPTION;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class test {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String url = "jdbc:mysql://127.0.0.1/?useSSL=false&user=root&password=mirim";
+        String userName = "root";
+        String password = "mirim";
+        Class.forName("com.mysql.jdbc.Driver");
+        System.out.println("드라이버 연결 성공!");
+        Connection connection = DriverManager.getConnection(url, userName, password);
+        Statement stmt = connection.createStatement();
+        String createSql = "CREATE DATABASE IF NOT EXISTS `AdaventureOfWhaleDB`";
+        stmt.executeUpdate(createSql);
+//        url+="/AdaventureOfWhaleDB";
+//        connection = DriverManager.getConnection(url, userName, password);
+//        stmt = connection.createStatement();
+//        createSql = "CREATE TABLE IF NOT EXISTS user_scoer_tabel(user_name VARCHAR(50), user_scoer INT)";
+//        stmt.execute(createSql);
+    }
+}
